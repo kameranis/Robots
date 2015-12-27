89d2a2ff817a410660d11e35136000f0d81bac0a
kameranis <kameranis@gmail.com>
2015-12-27 22:13:34 +0200

    Add testcases

--- tests/testgenerator.cpp
+++ tests/testgenerator.cpp
@@ -3,84 +3,84 @@
 #include <time.h>       /* time */
 #include <fstream>
 #include <cmath>
+
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
 
-  ofstream fout ("input.txt") ;
-  srand (time(NULL));
-  int n,m;
-  n=rand()%700;
-  m=rand()%700;
- /*char **a=new  char*[n];
-  char **b=new  char*[n];
-  for(int i=0;i<n;i++){
+    ofstream fout (argv[3]) ;
+    srand (time(NULL));
+    int N = atoi(argv[1]);
+    int M = atoi(argv[2]);
+    /*char **a=new  char*[n];
+      char **b=new  char*[n];
+      for(int i=0;i<n;i++){
       a[i]=new char[m];
       b[i]=new char[m];
-  }*/
-  char a[m][n],b[m][n];
-  int countX=0;
-  int ar;
-  for (int i=0;i<m;i++)
-    for (int j=0;j<n;j++)
-      if((ar=rand()%10) < 9) {
-        a[i][j]='O';
-        b[i][j]='O';
-      }
-      else {
-        a[i][j]='X';
-        b[i][j]='X';
-        countX++;
-      }
-// initial positions
-  int startx1,starty1,startx2,starty2;
-  do{
-    startx1=rand()%m;
-    starty1=rand()%n;
-    startx2=rand()%m;
-    starty2=rand()%n;
-  }
-  while(a[startx1][starty1]!='O'  || a[startx2][starty2]!='O' || (startx1==startx2 && starty1!=starty2));
+      }*/
+    char a[N][M],b[N][M];
+    int countX=0;
+    int ar;
+    for (int i=0;i<N;i++)
+        for (int j=0;j<M;j++)
+            if((ar=rand()%10) < 9) {
+                a[i][j]='O';
+                b[i][j]='O';
+            }
+            else {
+                a[i][j]='X';
+                b[i][j]='X';
+                countX++;
+            }
+    // initial positions
+    int startx1,starty1,startx2,starty2;
+    do{
+        startx1=rand() % N;
+        starty1=rand() % M;
+        startx2=rand() % N;
+        starty2=rand() % M;
+    }
+    while(a[startx1][starty1]!='O'  || a[startx2][starty2]!='O' || (startx1==startx2 && starty1!=starty2));
 
-  //final position
-  int endx,endy;
-  do{
-    endx=rand()%m;
-    endy=rand()%n;
-  }
-  while (a[endx][endy]!='O');
-
-
-
-// meeting points
-int meet = rand()% (n/2);
-int meeting1[600],meeting2[600];
-
-for (int i=0; i<meet;i++){
-  do{
-    meeting1[i]=rand()%m;
-    meeting2[i]=rand()%n;
-  }
-  while(b[meeting1[i]][meeting2[i]]!='O' && (meeting1[i]==endx && meeting2[i]==endy));
-  b[meeting1[i]][meeting2[i]]='C';
-}
+    //final position
+    int endx,endy;
+    do{
+        endx=rand() % N;
+        endy=rand() % M;
+    }
+    while (a[endx][endy]!='O');
 
 
-// printing
 
-fout << n << " " << m << endl;
-fout << starty1+1<< " "<< startx1+1<<endl;
-fout << starty2+1<< " "<< startx2+1<<endl;
-fout << endy+1 << " " << endx+1 << endl;
-fout << meet<<endl;
-for (int i=0;i<meet;i++) fout << meeting2[i]+1<< " "<<meeting1[i]+1<<endl;
-for (int i=0;i<m;i++){
-  for (int j=0;j<n;j++)
-    fout <<a[i][j];
-  fout << endl;
-}
-fout << endl << endl;
+    // meeting points
+    int meet = rand() % (N/2);
+    int meeting1[meet], meeting2[meet];
+
+    for (int i = 0; i < meet; i++){
+        do{
+            meeting1[i]=rand() % N;
+            meeting2[i]=rand() % M;
+        }
+        while(b[meeting1[i]][meeting2[i]]!='O' || (meeting1[i]==endx && meeting2[i]==endy));
+        b[meeting1[i]][meeting2[i]]='C';
+    }
 
 
-return 0;
+    // printing
+
+    fout << N << " " << M << endl;
+    fout << startx1+1<< " "<< starty1+1 << endl;
+    fout << startx2+1<< " "<< starty2+1 << endl;
+    fout << endx+1 << " " << endy+1 << endl;
+    fout << meet << endl;
+    for (int i=0;i<meet;i++) fout << meeting1[i]+1 << " " << meeting2[i]+1 << endl;
+    for (int i=0;i<N;i++){
+        for (int j=0;j<M;j++)
+            fout << a[i][j];
+        fout << endl;
+    }
+    fout << endl << endl;
+
+
+    return 0;
 }
